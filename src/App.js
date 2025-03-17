a408f310f2cf9ece0ba76ce23eea89a46d4e6d83
Samuel Safahi <60489622+samisbakedham@users.noreply.github.com>
2025-03-17 15:36:56 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import CheckoutPage from './components/CheckoutPage';
 import Dashboard from './components/Dashboard';
 import Users from './components/Users';
 import AuthCallback from './components/AuthCallback';
+import Auth from './components/Auth';
 
 function App() {
   return (
@@ -21,14 +22,13 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/loans/:id" element={<Loans />} />
           <Route path="/loans" element={<Loans />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
           <Route path="/apply-loan" element={<ApplyLoan />} />
           <Route path="/profile" element={<Profile />} />
           <Route path="/checkout" element={<CheckoutPage />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/users" element={<Users />} />
           <Route path="/auth/callback" element={<AuthCallback />} />
+          <Route path="/auth" element={<Auth />} />
         </Routes>
       </div>
     </Router>
